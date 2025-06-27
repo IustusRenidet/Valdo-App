74be22b2984e8dbde1df76489928df9aa27037c6
Frida Sophia Monjaraz Guevara <iustusrenidet@gmail.com>
2025-06-27 00:40:33 -0600

    Integrar firebase

--- firebase.js
+++ firebase.js
@@ -1,16 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: reemplaza los siguientes valores con la configuración
-// de tu proyecto de Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'TU_API_KEY',
-  authDomain: 'TU_AUTH_DOMAIN',
-  projectId: 'TU_PROJECT_ID',
-  storageBucket: 'TU_STORAGE_BUCKET',
-  messagingSenderId: 'TU_SENDER_ID',
-  appId: 'TU_APP_ID'
+  apiKey: "AIzaSyCFT7fuT3bgxSaKT6PuRiYK23ZEoM0DKT4",
+  authDomain: "valdo-d6d32.firebaseapp.com",
+  projectId: "valdo-d6d32",
+  storageBucket: "valdo-d6d32.firebasestorage.app",
+  messagingSenderId: "911412113065",
+  appId: "1:911412113065:web:3b7c0b29fe2b8e627b83b8",
+  measurementId: "G-2XYHV1LTEZ"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
